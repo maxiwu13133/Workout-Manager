84fa771f8c7ba760db285e65c94dfdf1aff251a8
max <maximilianwu0828@gmail.com>
2024-06-13 12:10:00 -0700

    server basics and environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // express app
 const app = express()
 
+// routes
+app.get('/', (req, res) => {
+  res.json({msg: 'Welcome to the app'})
+})
+
+app.get('/home', (req, res) => {
+  res.json({msg: 'work in progress'})
+})
+
 // listen for requests through port number
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log('listening on port 4000')
 })
