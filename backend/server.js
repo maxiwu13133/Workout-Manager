6ae393e8561df97cc8f08c041fd5f416881a45bc
max <maximilianwu0828@gmail.com>
2024-06-15 14:39:39 -0700

    model and controllers started

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
 // routes
-app.get('/', (req, res) => {
-  res.json({msg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-app.get('/home', (req, res) => {
-  res.json({msg: 'work in progress'})
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests through port number
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db and listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
-// listen for requests through port number
-app.listen(process.env.PORT, () => {
-  console.log('listening on port 4000')
-})
