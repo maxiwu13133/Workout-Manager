c0d0fd24209e6ee079ee313b9f5f6f0ae72b3bbc
max <maximilianwu0828@gmail.com>
2024-06-30 17:22:40 -0700

    assign workouts to user

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,23 +4,24 @@ const mongoose = require('mongoose')
 
 // get all workouts
 const getWorkouts = async (req, res) => {
-  const workout = await Workout.find({}).sort({createdAt: -1})
+  const user_id = req.user._id
+  const workout = await Workout.find({ user_id }).sort({ createdAt: -1 })
   res.status(200).json(workout)
 }
 
 
 // get a workout
 const getWorkout = async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({error: 'No such workout'})
+    return res.status(404).json({ error: 'No such workout' })
   }
 
   const workout = await Workout.findById(id)
 
   if (!workout) {
-    return res.status(404).json({error: 'No such workout'})
+    return res.status(404).json({ error: 'No such workout' })
   }
 
   res.status(200).json(workout)
@@ -29,7 +30,7 @@ const getWorkout = async (req, res) => {
 
 // post a workout
 const createWorkout = async (req, res) => {
-  const {title, reps, load} = req.body
+  const { title, reps, load } = req.body
 
   let emptyFields = []
 
@@ -48,10 +49,11 @@ const createWorkout = async (req, res) => {
 
   // add doc to db
   try {
-    const workout = await Workout.create({title, reps, load})
+    const user_id = req.user._id
+    const workout = await Workout.create({ title, reps, load, user_id })
     res.status(200).json(workout)
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message })
   }
 }
 
